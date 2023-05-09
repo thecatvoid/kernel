2f376e14525bcae236005524bde3e6dbe6db6f5c
thecatvoid <voidcat@tutanota.com>
2023-05-09 13:18:59 +0530

    update

--- kernel.sh
+++ kernel.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 set -e
 flags=(
-        IGNORE_CC_MISMATCH='1'
-        CC='gcc-13'
-        AS='as'
-        CFLAGS='-march=haswell -mtune=haswell -mcmodel=small -O2 -pipe -ftree-vectorize -fprofile-generate -fomit-frame-pointer -fno-ident'
-        ARCH='x86'
-        SUBARCH='x86'
-        AR='gcc-ar-13'
-        RANLIB='gcc-ranlib-13'
-        LD='ld'
-        KBUILD_LDFLAGS=''
-        ARCH='x86'
-        NM='gcc-nm-13'
-        OBJCOPY='objcopy'
-        READELF='readelf'
-        HOSTCC='gcc-13'
-        OBJSIZE='size'
-        OBJDUMP='objdump'
-        STRIP='strip'
-        READELF='readelf'
-        HOSTAR='gcc-ar-13'
-        HOSTLD='ld'
+       CC='clang-17'
+       LLVM='1'
+       LLVM_IAS='1'
+       AS='llvm-as-17'
+       CFLAGS='-march=haswell -mtune=haswell -O2 -pipe -ftree-vectorize -flto=full -fprofile-generate -fomit-frame-pointer -fno-ident'
+       ARCH='x86'
+       SUBARCH='x86'
+       AR='llvm-ar-17'
+       RANLIB='llvm-ranlib-17'
+       LD='ld.lld-17'
+       KBUILD_LDFLAGS=''
+       NM='llvm-nm-17'
+       OBJCOPY='llvm-objcopy-17'
+       READELF='llvm-readelf-17'
+       HOSTCC='clang-17'
+       OBJSIZE='llvm-size-17'
+       OBJDUMP='llvm-objdump-17'
+       STRIP='llvm-strip-17'
+       READELF='llvm-readelf-17'
+       HOSTCXX='clang++-17'
+       HOSTAR='llvm-ar-17'
+       HOSTLD='ld.lld-17'
 )
+
 declare -g "${flags[@]}"
 export "${flags[@]}"
 
@@ -52,15 +54,16 @@ setup_build() {
         home
         apt update
         apt install -t experimental -y \
-                bc make libncurses5-dev libssl-dev \
-                bison binutils-common binutils flex libelf-dev \
-                gcc-13 gcc-13-base gcc-13-multilib gcc-13-plugin-dev \
-                libgcc-13-dev binutils-dev gzip xz-utils curl \
-                fakeroot wireless-regdb lz4 git busybox-static zstd cpio
+                git lz4 linux-headers-amd64 \
+                build-essential bc libncurses5-dev libssl-dev \
+                bison flex libelf-dev libclang1-17 libclang-cpp17-dev \
+                libclang-cpp17 clangd-17 clang-tools-17 clang-17 \
+                llvm-17-tools llvm-17 llvm-17-runtime llvm-17-dev \
+                llvm-17-linker-tools lld-17 \
+                fakeroot wireless-regdb xz-utils
 
         [[ -d ./linux ]] && git clone --depth=1 https://github.com/zen-kernel/zen-kernel ./linux
-        cp -f linux_defconfig ./linux/arch/x86/configs/linux_defconfig
-
+        cp -f ./linux_defconfig ./linux/arch/x86/configs/
         cd linux
         version=$(grep "^VERSION" Makefile | awk '{print $3}')
         patchlevel=$(grep "^PATCHLEVEL" Makefile | awk '{print $3}')
